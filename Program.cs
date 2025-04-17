e8b6bb17bf4a388828a0ea5b6708c48e792cd905
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-04-16 19:56:10 -0600

    loggin

--- Program.cs
+++ Program.cs
@@ -3,13 +3,23 @@
 
 // loadBankCustomers() saveBankCustomers() validateUser() checkBalance() withdrawMoney() depositMoney() quickWithdraw(int Amount) displayTranactions()
 
-//Loading Users
-
-
 
 Dictionary<string, List<string>> users = LoadBankCustomers();
+Console.Clear();
 
-Console.WriteLine(users["jallen"][2]);
+(bool access, string name) = validateUser(users);
+
+if (access)
+{
+Console.WriteLine(access);
+Console.WriteLine(name);
+}
+else
+{
+    Console.WriteLine("Error please try again later -To many failed attempts");
+}
+
+
 
 
 //Loads Users from text file
@@ -22,9 +32,51 @@ Console.WriteLine(users["jallen"][2]);
 
     foreach (string line in tempReadLines)
     {
+        List<string> trim = new List<string> ();
         string[] temp = line.Split(",");
-        Users.Add(temp[0], new List<string>(temp));
+        foreach (string word in temp)
+        {
+        trim.Add(word.Trim());
+        }
+        
+        Users.Add(trim[0], new List<string>(trim));
     }
 
     return Users;
+}
+
+// login system
+(bool,string) validateUser(Dictionary<string, List<string>> users)
+{
+    do
+    {
+    Console.Write("What is your Username :");
+    string? nametry = Console.ReadLine();
+
+    if (users.ContainsKey(nametry))
+    {
+        int trys = 0;
+        while (trys < 3)
+        {
+        Console.Write("What is your Pin :");
+        
+        Int32.TryParse(Console.ReadLine() ,out int pintry);
+
+        int pin = Convert.ToInt32(users[nametry][1]);
+        if (pintry == pin)
+        {
+            return (true, nametry);
+        }
+        else 
+        {
+            trys++;
+            Console.WriteLine("Incorrect Pin");
+        }
+        }
+        
+        return (false, "BLOCKED");
+    }
+    Console.WriteLine("Hmmm?");
+
+    } while (true);
 }
